da7fc9adce9a9909f155930af4d0d8bef20d4b2b
visi <visiface@live.com>
2021-01-31 01:56:14 +0100

    added SWITCH statements to SIGNUP

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # crumbly
-School project for PHP
-
-## IT GON BE FUUUUUUUN
-heeeeeell yeeeeeeee
+School project for PHP by Kimberly Karlsson 
+a site designed for being a recipe compendium as well as a "what up" to other bakers.
--- signup.php
+++ signup.php
@@ -7,6 +7,7 @@
 
 
   <section>
+
     <h2>Sign Up</h2>
     <form action="includes/signup.inc.php" method="post">
       <input type="text" name="username" placeholder="username">
@@ -17,7 +18,32 @@
     </form>
   </section>
 
-  <!-- TBA error messages -->
+  <?php 
+  if( isset($_GET["error"]) ){
+
+    switch ($_GET["error"]) {
+      case "emptyinput":
+        echo "Please fill in all fields.";
+        break;
+      case "invalidUID":
+        echo "Invalid username. Please enter a valid username.";
+        break;
+      case "invalidEmail":
+        echo "Invalid email. Please enter a valid email.";
+        break;
+      case "invalidPassword":
+        echo "Passwords do not match. Please try again. Remember: case sensitive and special characters need to match!";
+        break;
+      case "usernameTaken":
+        echo "This username is taken. Please enter another username!";
+        break;
+      case "none":
+        echo "Sign-Up Successful! Feel free to login.";
+        break;
+    }
+
+  }
+?>
 
 
 <?php include_once("footer.php"); ?>
